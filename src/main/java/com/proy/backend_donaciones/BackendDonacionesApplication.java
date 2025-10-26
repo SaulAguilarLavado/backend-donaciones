6d3b24c1d120353841dcda0370b1413166a3509c
SaulAguilarLavado <u22304350@utp.edu.pe>
2025-10-26 14:36:33 -0500

    Backend

---
+++ src/main/java/com/proy/backend_donaciones/BackendDonacionesApplication.java
@@ -0,0 +1,13 @@
+package com.proy.backend_donaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDonacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDonacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proy/backend_donaciones/BackendDonacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proy.backend_donaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDonacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
